2b0d8d560f14fe8d09c4d5f2e0d35ace854f8a66
Bekir Kökkuş <bekirkokkus@gmail.com>
2017-11-12 05:15:21 +0300

    Network IP Mac Addr
    Display all ip addr in pc.

---
+++ Network_IpAdresMacAdres/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace Network_IpAdresMacAdres
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (NetworkInterface networkInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                var macAddress = networkInterface.GetPhysicalAddress().ToString();
+                if (macAddress != string.Empty)
+                {
+                    lstListe.Items.Add("Mac Adres : " + macAddress);
+                }
+            }
+
+            IPAddress[] ipHostAdress = Dns.GetHostAddresses(Dns.GetHostName());
+
+            for (int i = 0; i < ipHostAdress.Length; i++)
+            {
+                lstListe.Items.Add("Local IP Adres : " + ipHostAdress[i].ToString());
+            }
+        }
+    }
+}
